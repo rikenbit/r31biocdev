1d3774541f6168c5bfd03b460ebe84dbc3a96294
Manabu ISHII <manabu.ishii.rb@gmail.com>
2015-10-09 10:57:05 +0900

    test case update

--- test/integration/default/minitest/test_default.rb
+++ test/integration/default/minitest/test_default.rb
@@ -13,7 +13,7 @@ describe 'check Bioconductor version' do
   it "check Bioconductor version" do
     system('/usr/local/R-devel/bin/R CMD BATCH showBioconductorVersion.R')
     # develop
-    assert system('grep "Bioconductor version 3.1 (BiocInstaller 1.18.4)" showBioconductorVersion.Rout'), 'Bioconductor version is not expected version. maybe Bioconductor package is updated'
+    assert system('grep "Bioconductor version 3.2 (BiocInstaller 1.19.14)" showBioconductorVersion.Rout'), 'Bioconductor version is not expected version. maybe Bioconductor package is updated'
     # stable
     #assert system('grep "Bioconductor version 2.14 (BiocInstaller 1.14.2)" showBioconductorVersion.Rout'), 'Bioconductor version is not expected version. maybe Bioconductor package is updated'
   end
